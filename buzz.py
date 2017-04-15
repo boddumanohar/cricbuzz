c9897312345adb99bd9187f944c2727a538b9fce
boddumanohar <201451024@iiitvadodara.ac.in>
2017-04-15 17:53:39 +0530

    code lint

--- buzz.py
+++ buzz.py
@@ -2,7 +2,10 @@ from pycricbuzz import Cricbuzz
 import json
 import time
 from twilio.rest import TwilioRestClient
-client = TwilioRestClient("AC0b85724324030f71484dd878fd6ec27f", "f700b474f7f686b2ad88ffcaa73118b6")
+
+client = TwilioRestClient( "AC0b85724324030f71484dd878fd6ec27f", 
+			   "f700b474f7f686b2ad88ffcaa73118b6" 
+			 )
 
 def buzz():
 	c = Cricbuzz()
@@ -15,11 +18,6 @@ def buzz():
 	l.append(h['batting']['team'])
 	l.append(h['batting']['score'][0]['overs']+"overs")
 	l.append(h['batting']['score'][0]['runs']+"/"+h['batting']['score'][0]['wickets'])
-
-	#l.append(h['bowling']['team'])
-	#l.append(h['bowling']['score'][0]['overs']+"overs")
-	#l.append(h['bowling']['score'][0]['runs']+"/"+h['bowling']['score'][0]['wickets'])
-
 
 	summary=" ".join(l)
 
@@ -41,7 +39,6 @@ def buzz():
 	com=c.commentary(['1'])
 	for i in range(2):
 		com_list.append(com['commentary'][i])
-	
 	result.append(summary)
 	result.append(m)
 	result.append(bow)
@@ -49,34 +46,10 @@ def buzz():
 	return(result)
 
 
-
-
-
-
-
-
-	
 while(True):
 	only=buzz()
 	print(only)
 	client.messages.create(to="+919978762997", from_="+14847274317",body=only[0])
 	client.messages.create(to="+919978762997", from_="+14847274317",body=only[1])
 	client.messages.create(to="+919978762997", from_="+14847274317",body=only[2])
-	
-	
-	
 	time.sleep(1)	
-
-
-
-# we import the Twilio client from the dependency we just installed
-
-
-# the following line needs your Twilio Account SID and Auth Token
-
-
-# change the "from_" number to your Twilio number and the "to" number
-# to the phone number you signed up for Twilio with, or upgrade your
-# account to send SMS to any phone number
-#pabba 9978762997
-#client.messages.create(to="+919898272433", from_="+14847274317",body=a)
